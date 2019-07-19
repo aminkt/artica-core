6b66e50b9f2e3e45019986010b1753c8c18ea429
Amin Keshavarz <ak_1596@yahoo.com>
2019-07-19 14:22:31 +0430

    Fix env

---
+++ src/Entities/Entity.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Artica\Entities;
+
+use yii\db\ActiveRecord;
+
+/**
+ * Class Entity
+ * This class contain database logic.
+ *
+ * @author  Amin Keshavarz <ak_1596@yahoo.com>
+ * @package Artica\Entities
+ *
+ */
+abstract class Entity extends ActiveRecord
+{
+
+}
