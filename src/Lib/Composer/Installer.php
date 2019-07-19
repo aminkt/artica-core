4fd92eb7c7d2d0cb87d1757501e5f41607b31eee
Amin Keshavarz <ak_1596@yahoo.com>
2019-07-19 16:06:42 +0430

    Fix env

--- src/Lib/Composer/Installer.php
+++ src/Lib/Composer/Installer.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Artica\Lip\Composer;
+namespace Artica\Lib\Composer;
 
 use yii\composer\Installer as YiiInstaller;
 
@@ -21,16 +21,16 @@ class Installer extends YiiInstaller
         $dotenv = \Dotenv\Dotenv::create(dirname(__DIR__));
         $dotenv->load();
 
-        Composer::initApp($configs['app_config_file']);
-        Composer::initDb($configs['db_config_file']);
-        Composer::initRedis();
-        Composer::initElasticSearch();
+        Installer::initApp($configs['app_config_file']);
+        Installer::initDb($configs['db_config_file']);
+        Installer::initRedis();
+        Installer::initElasticSearch();
     }
 
     private static function initApp(string $configFile): void
     {
         $file = __DIR__ . '/api/config/main.php';
-        Composer::changeFrameworkConfig($file, 'language', getenv('APP_LANGUAGE'));
+        Installer::changeFrameworkConfig($file, 'language', getenv('APP_LANGUAGE'));
         echo "   Application configuration finished.\n";
     }
 
@@ -38,21 +38,21 @@ class Installer extends YiiInstaller
     {
         $file = dirname(__FILE__) . '/common/config/conf.d/db.php';
         $dsn = getenv('DB_CONNECTION') . ':host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_DATABASE');
-        changeFrameworkConfig($file, 'dsn', $dsn);
-        changeFrameworkConfig($file, 'username', getenv('DB_USERNAME'));
-        changeFrameworkConfig($file, 'password', getenv('DB_PASSWORD'));
-        changeFrameworkConfig($file, 'tablePrefix', getenv('DB_TABLE_PREFIX'));
+        Installer::changeFrameworkConfig($file, 'dsn', $dsn);
+        Installer::changeFrameworkConfig($file, 'username', getenv('DB_USERNAME'));
+        Installer::changeFrameworkConfig($file, 'password', getenv('DB_PASSWORD'));
+        Installer::changeFrameworkConfig($file, 'tablePrefix', getenv('DB_TABLE_PREFIX'));
         echo "   Database configuration finished.\n";
     }
 
     private static function initRedis()
     {
-        printError("Redis configuration implemented");
+        echo ("Redis configuration implemented");
     }
 
     private static function initElasticSearch()
     {
-        printError("Es configuration Not implemented");
+        echo ("Es configuration Not implemented");
     }
 
     private static function changeFrameworkConfig(string $file, string $key, string $val): void
