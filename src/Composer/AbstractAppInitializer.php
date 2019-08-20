3fa215dc87922e54bad12713286be1cdd9366810
Amin Keshavarz <ak_1596@yahoo.com>
2019-08-20 14:43:25 +0430

    Fix new namespace

--- src/Composer/AbstractAppInitializer.php
+++ src/Composer/AbstractAppInitializer.php
@@ -15,6 +15,21 @@ abstract class AbstractAppInitializer
     protected $vendorPath;
     protected $projectPath;
 
+    private $_foregroundColors = [
+        'black' => '0;30',
+        'blue' => '0;34',
+        'green' => '0;32',
+        'red' => '0;31',
+        'yellow' => '1;33',
+        'white' => '1;37',
+    ];
+    private $_backgroundColors = [
+        'black' => '40',
+        'red' => '41',
+        'green' => '42',
+        'yellow' => '43',
+    ];
+
     public function __construct()
     {
         $this->initApplication();
@@ -89,5 +104,63 @@ abstract class AbstractAppInitializer
     {
         $content = preg_replace('/([\'"]' . $key . '[\'"]\s*=>\s*)([\'"].*[\'|"])/', "\\1'$val'", file_get_contents($file));
         file_put_contents($file, $content);
+    }
+
+    /**
+     * Print error message.
+     *
+     * @param string $message
+     *
+     * @return void
+     *
+     * @author Amin Keshavarz <ak_1596@yahoo.com>
+     */
+    protected function printError(string $message): void
+    {
+        $this->printMessage($message, 'red', 'black');
+    }
+
+    /**
+     * Print success messages.
+     *
+     * @param string $message
+     *
+     * @return void
+     *
+     * @author Amin Keshavarz <ak_1596@yahoo.com>
+     */
+    protected function printSuccess(string $message): void
+    {
+        $this->printMessage($message, 'green', 'black');
+    }
+
+    /**
+     * Print colored message.
+     *
+     * @param string      $message
+     * @param string|null $foregroundColor
+     * @param string|null $backgroundColor
+     *
+     * @return void
+     *
+     * @author Amin Keshavarz <ak_1596@yahoo.com>
+     */
+    protected function printMessage(string $message, ?string $foregroundColor = null, ?string $backgroundColor = null): void
+    {
+        $colored_string = "";
+
+        // Check if given foreground color found
+        if (isset($this->foreground_colors[$foregroundColor])) {
+            $colored_string .= "\033[" . $this->_foregroundColors[$foregroundColor] . "m";
+        }
+        // Check if given background color found
+        if (isset($this->background_colors[$backgroundColor])) {
+            $colored_string .= "\033[" . $this->_backgroundColors[$backgroundColor] . "m";
+        }
+
+        // Add string and end coloring
+        $colored_string .=  $message . "\033[0m";
+
+        echo $colored_string;
     }
 }
