bc450c6ae6cbf2c25773da27a5197afe4174bcac
Amin Keshavarz <ak_1596@yahoo.com>
2020-02-13 19:13:06 +0330

    Sending email and sms to customers.

--- src/Services.php
+++ src/Services.php
@@ -9,8 +9,10 @@ use Artica\Lib\SMS\SMSInterface;
 use DateTimeZone;
 use Yii;
 use yii\base\Security;
+use yii\base\View;
 use yii\caching\CacheInterface;
 use yii\i18n\Formatter;
+use yii\log\Logger;
 use yii\mail\MailerInterface;
 use yii\queue\amqp_interop\Queue;
 use yii\queue\redis\Queue as RedisQueue;
@@ -71,14 +73,6 @@ abstract class Services
     }
 
     /**
-     * @return SMSInterface
-     */
-    public static function serviceSMS(): SMSInterface
-    {
-        return Yii::$app->get('sms');
-    }
-
-    /**
      * @return Formatter
      */
     public static function serviceFormatter(): Formatter
@@ -107,5 +101,21 @@ abstract class Services
     {
         $logger = new ErrorLogger();
         return $logger;
+    }
+
+    /**
+     * @return Logger
+     */
+    public static function serviceLog(): Logger
+    {
+        return Yii::getLogger();
+    }
+
+    /**
+     * @return \yii\base\View
+     */
+    public static function serviceHtmlRender(): View
+    {
+        return Yii::$app->getView();
     }
 }
